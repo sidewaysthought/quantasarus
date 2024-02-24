180467ed20831b5d7ee97e1868e3cd7d9f76a90b
David D. <david@sidewaysthought.net>
2024-02-24 16:45:21 -0600

    Add download function and update main() in quantasarus.py

--- quantasarus.py
+++ quantasarus.py
@@ -1,8 +1,32 @@
 import argparse
-from huggingface_hub import hf_hub_download
+import os
+from huggingface_hub import hf_hub_download, snapshot_download
 
-def download(model):
-    pass
+BASE_MODEL_DIR = "models"
+
+def download(repo, file=""):
+
+    # If the model dir doesn't exist, create it
+    if not os.path.exists("models"):
+        os.makedirs("models")
+
+    if file != "":
+        # Split the repo on the / to get the owner and model name
+        repo_split = repo.split("/")
+        owner = repo_split[0]
+        model = repo_split[1]
+        dest_path = os.path.join(BASE_MODEL_DIR, owner, model)
+    else:
+        dest_path = os.path.join(BASE_MODEL_DIR, owner)
+
+    try:
+        if file == "":
+            snapshot_download(repo_id=repo, local_dir=dest_path)
+        else:
+            hf_hub_download(repo_id=repo, filename=file, local_dir=dest_path)
+    except Exception as e:
+        print(f"Error: {e}")
+        exit()
 
 def quatize_gguf(model):
     pass
@@ -23,6 +47,7 @@ def validate_args(args):
 def main():
     args = setup_args()
     arg_values = validate_args(args)
+    downloaded = download(arg_values["repo"], arg_values["file"])
 
 if __name__ == "__main__":
     main()
